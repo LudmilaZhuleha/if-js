32cc1c84d32c31f7336aa83a2cf662496c18996c
LudmilaZhuleha <zhuleha@lir.by>
2022-05-31 21:19:57 +0300

    lesson 8: draft to class Students

--- src/data/data.js
+++ src/data/data.js
@@ -197,4 +197,30 @@ const data = [
         hotel: 'Hotel Rehberge Berlin Mitte',
     },
 ];
-export {hotels, data};
+const studentsData = [
+    {
+        firstName: 'Василий',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Java',
+    },
+    {
+        firstName: 'Иван',
+        lastName: 'Иванов',
+        admissionYear: 2018,
+        courseName: 'JavaScript',
+    },
+    {
+        firstName: 'Александр',
+        lastName: 'Федоров',
+        admissionYear: 2017,
+        courseName: 'Python',
+    },
+    {
+        firstName: 'Николай',
+        lastName: 'Петров',
+        admissionYear: 2019,
+        courseName: 'Android',
+    }
+];
+export {hotels, data, studentsData};
--- src/main.js
+++ src/main.js
@@ -1,62 +1,47 @@
-import { hotels, data } from "./data/data.js";
+import { hotels, data, studentsData } from "./data/data.js";
 
-//Create function checking palindrome string for true/false
-const palindrome = (str) => str === str.split('').reverse().join('');
-
-console.log(palindrome('molom'));
-
-//Create a function that receives an array and a string and checks if there are any matches of string in any array
-// value. The function should return a string with all findings if any.
-const searchMatches = (arr, str) => {
-  if(Boolean(!str) || typeof str !== 'string') return `Data input error`;
-  const newArr = [];
-  arr.forEach((item) => {
-    const hotelItem = Object.values(item).join('').toLowerCase();
-    if (hotelItem.includes(str)) newArr.push(item);
-    })
-  return !newArr.length ? `Sorry, no matches found! Try another destination`: newArr;
-}
-console.log(searchMatches(data, 'rus'));
-
-//Create a function that finds unique countries with all matching cities in these countries
-const findUniqueCountries = (array) => {
-  const arrayUnique = array.reduce((acc, item) => {
-    acc[item.country] = acc[item.country] || [];
-    acc[item.country].push(item.city);
-    return acc;
-  }, {});
-  return arrayUnique;
-};
-
-console.log(findUniqueCountries(hotels));
-
-//Create a function getCalendarMonth, which receives parameters:
-//daysInMonth =30,
-//daysInWeek =7,
-//weeksInMonth=5,
-//and any day in week from 0 to 6 to start the month from.
-//The function should return the array of arrays illustrating calendar month.
-
-const getCalendarMonth = (daysInMonth, daysInWeek, dayOfWeek) => {
-  if (
-    typeof daysInMonth !== 'number' ||
-    typeof daysInWeek !== 'number' ||
-    typeof dayOfWeek !== 'number' ||
-    dayOfWeek > 7
-  )
-    return 'Data input failure';
-  const arr = [];
-  let counter = daysInMonth - dayOfWeek + 1;
-  for (let i = 0; i < 5; i++) {
-    arr[i] = [];
-    for (let j = 0; j < daysInWeek; j++) {
-      if (counter === 31) counter = 1;
-      arr[i][j] = 1;
-      arr[i][j] = counter++;
+class User {
+    constructor(options) {
+        this.firstname = options.firstname;
+        this.lastname = options.lastname;
     }
-  }
-  return arr;
-};
-console.log(getCalendarMonth(30, 7, 1));
+    get fullname(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
+const user = new User({firstname:'Andi', lastname: 'Iva'})
+console.log(user.fullname);
 
-export { findUniqueCountries, palindrome, getCalendarMonth };
+class Student extends User{
+    constructor(options) {
+        super(options);
+        this.admissionyear = options.admissionyear;
+        this.coursename = options.coursename
+    }
+    get course(){
+        const currentyear = 2022
+        return currentyear - this.admissionyear
+    }
+}
+const student = new Student({firstname: 'Ola', lastname: 'Kli', admissionyear:2019, coursename: 'BE'})
+console.log(student.course)
+
+class Students {
+    constructor (arr){
+        this.arr = arr;
+    }
+}
+ Students.prototype.getInfo = function(){
+    return this.arr.sort((a,b)=>
+    {
+        const B = new Student(b).course;
+        const A = new Student(a).course;
+        if(A > B) return 1
+        else if (B>A) return -1
+        else return 0
+    }).map(item=> `${item.fullname} - ${item.coursename}, ${item.course} course`);
+}
+
+
+const sorted = new Students(studentsData);
+console.log(sorted.getInfo())
