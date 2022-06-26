6aebccca1638beeee7fed1d7b0fc267ed43c41d7
LudmilaZhuleha <zhuleha@lir.by>
2022-06-26 22:42:36 +0300

    lesson 9: fix: colorChange() refactored

--- src/data/iteratorChangeColor.js
+++ src/data/iteratorChangeColor.js
@@ -1,36 +1,36 @@
 const colors = {
-    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
-    [Symbol.iterator]() {
-        this.counter = 0;
-        return this; // ваш код
-    },
-    next() {
-        if (this.current === undefined) {
-            this.current = this.counter;
-        }
-        if (this.current <= this.data.length) {
-            return {
-                done: false,
-                value: this.data[this.current++],
-            };
-        } else {
-            delete this.current;
-            this.current = 0;
-            return {
-                done: false,
-                value: this.data[this.current++],
-            };
-        }
-    },
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  [Symbol.iterator]() {
+    this.counter = 0;
+    return this; // ваш код
+  },
+  next() {
+    if (this.current === undefined) {
+      this.current = this.counter;
+    }
+    if (this.current <= this.data.length) {
+      return {
+        done: false,
+        value: this.data[this.current++],
+      };
+    } else {
+      delete this.current;
+      this.current = 0;
+      return {
+        done: false,
+        value: this.data[this.current++],
+      };
+    }
+  },
 };
 
 function changeColor() {
-    return function (event) {
-        event.target.style.backgroundColor = colors.next().value;
-    };
+  return function (event) {
+    event.target.style.backgroundColor = colors.next().value;
+  };
 }
 const ids = document.querySelectorAll('p');
 
 for (let i = 0; i < ids.length; i++) {
-    ids[i].addEventListener('click', changeColor());
+  ids[i].addEventListener('click', changeColor());
 }
