ffaa931a4222585c9db66376cd9e7e9946aaa49c
LudmilaZhuleha <zhuleha@lir.by>
2022-07-09 00:46:48 +0300

    lesson 12: fix: use separate getData function

--- src/main.js
+++ src/main.js
@@ -1,31 +1,40 @@
 const homesCards = document.querySelector('.homes-cards');
 
-const createElement =(tagName, className)=>{
-    const elem = document.createElement(tagName);
-    elem.classList.add(className);
-    return elem;
-}
+const createElement = (tagName, className) => {
+  const elem = document.createElement(tagName);
+  elem.classList.add(className);
+  return elem;
+};
 
-const render = async()=>{
-    const url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
+const url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
+
+const render = (data) => {
+  data.forEach((item) => {
+    const { name, city, country, imageUrl } = item;
+    const homesCard = createElement('div', 'homes-card');
+    const img = createElement('img', 'homes-image');
+    img.src = imageUrl;
+    img.alt = `${name}`;
+    img.width = '295';
+    img.height = '295';
+
+    const title = createElement('h3', 'homes-image-name');
+    title.textContent = `${name}`;
+
+    const location = createElement('p', 'homes-image-location');
+    location.textContent = `${city}, ${country}`;
+
+    homesCard.append(img, title, location);
+    homesCards.append(homesCard);
+  });
+};
+const getData = async (url) => {
+  try {
     const response = await fetch(url);
     const result = await response.json();
-    result.forEach((item)=>{
-        const {name, city, country, imageUrl} = item;
-        const homesCard = createElement('div', 'homes-card');
-        const img = createElement('img', 'homes-image');
-        img.src = imageUrl;
-        img.alt = `${name}`;
-        img.width = '295';
-        img.height ='295';
-        const title = createElement('h3', 'homes-image-name');
-        title.textContent = `${name}`;
-
-        const location = createElement('p', 'homes-image-location');
-        location.textContent = `${city}, ${country}`;
-
-        homesCard.append(img, title, location);
-        homesCards.append(homesCard);
-    })
-}
-render();
+    render(result);
+  } catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
+};
+getData(url);
