032681cca7abbce18b432a97b9bfea3f475782d2
LudmilaZhuleha <zhuleha@lir.by>
2022-07-15 18:58:21 +0300

    Lesson 15: Add file sending.

--- src/main.js
+++ src/main.js
@@ -1,28 +1,18 @@
-const formElem = document.querySelector('.form-wrapper');
-// const submitBtn = document.querySelector('.form-button');
-const urlPost = 'https://fe-student-api.herokuapp.com/api/file';
+const form = document.querySelector('.form-wrapper');
+const url = 'https://fe-student-api.herokuapp.com/api/file';
 
-formElem.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const inputedFile = document.querySelector('.form-input').files[0];
-
-  console.log(inputedFile);
-  const formData = new FormData();
-  formData.append('file', inputedFile);
-
-  console.log(formData);
-
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
   const fetchOptions = {
     method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-    body: formData,
+    enctype: 'multipart/form-data',
+    body: new FormData(form),
   };
-  const response = await fetch(urlPost, fetchOptions);
-  if (!response.ok) throw new Error(`URL not found: error type: ${response.status}`);
-
-  const result = await response.json();
-  console.log(result.message);
+  try {
+    const response = await fetch(url, fetchOptions);
+    const result = await response.json();
+    console.log(`File ${result.fileName} has been successfully uploaded.`);
+  } catch (error) {
+    console.log(error);
+  }
 });
